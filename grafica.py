2c52a874f18a42793647c0bd335860eea9e6c4ca
andreti03 <andreti03@gmail.com>
2021-05-20 13:50:44 -0500

    GUI

---
+++ grafica.py
@@ -0,0 +1,162 @@
+import pygame
+import requests
+
+WIDTH = 550
+background_color = (251,247,245)
+original_grid_element_color = (52, 31, 151)
+buffer = 5
+
+response = {"board":[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]}
+grid = response['board']
+grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+
+def insert(win, position):
+    i,j = position[1], position[0]
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.KEYDOWN:
+                if(grid_original[i-1][j-1] != 0):
+                    return
+                if(event.key == 48): #checking with 0
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    pygame.display.update()
+                    return
+                if(0 < event.key - 48 <10):  #We are checking for valid input
+                    pygame.draw.rect(win, background_color, (position[0]*50 + buffer, position[1]*50+ buffer,50 -2*buffer , 50 - 2*buffer))
+                    value = myfont.render(str(event.key-48), True, (0,0,0))
+                    win.blit(value, (position[0]*50 +15, position[1]*50))
+                    grid[i-1][j-1] = event.key - 48
+                    pygame.display.update()
+                    return
+                return
+        
+
+def list2dic(lista):
+    count=0
+    re=[]
+    res=[]
+    for i in lista:
+        re.append(i)
+        if count != 8:
+            count+=1
+        else:
+            count=0
+            res.append(re)
+            re=[]
+            
+    return res
+
+
+
+def dif(win,dif):
+    #FACIL
+    numeros1=[0,3,9,0,5,0,1,7,0,
+            0,0,5,0,6,0,8,0,0,
+            8,0,0,4,0,3,0,0,9,
+            0,1,8,0,0,0,3,5,0,
+            6,0,0,1,0,5,0,0,7,
+            0,7,2,0,0,0,9,6,0,
+            3,0,0,5,0,9,0,0,2,
+            0,0,1,0,3,0,4,0,0,
+            0,9,4,0,7,0,6,3,0]
+
+    #INTERMEDIO
+    numeros2=[8,0,0,7,0,9,5,0,0,
+            0,0,6,0,0,8,0,3,0,
+            0,0,0,0,4,5,0,0,0,
+            0,0,0,0,0,3,0,0,5,
+            9,0,5,0,0,0,6,0,3,
+            7,0,0,9,0,0,0,0,0,
+            0,0,0,5,8,0,0,0,0,
+            0,6,0,4,0,0,8,0,0,
+            0,0,2,6,0,7,0,0,9]  
+
+    #DIFICIL
+    numeros3=[0,0,0,0,4,0,0,5,0,
+            5,0,4,6,0,3,8,0,0,
+            0,3,0,0,0,5,0,7,0,
+            0,6,2,0,0,0,0,3,0,
+            8,0,3,0,0,0,7,0,9,
+            0,7,0,0,0,0,6,2,0,
+            0,5,0,4,0,0,0,8,0,
+            0,0,6,2,0,7,5,0,3,
+            0,8,0,0,5,0,0,0,0]
+
+    if dif =='1':
+        new = {"board":list2dic(numeros1)}
+    elif dif == '2':
+        new = {"board":list2dic(numeros2)}
+    elif dif == '3':
+        new = {"board":list2dic(numeros3)}
+
+    grid = new['board']
+    grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+    pygame.display.set_caption("Sudoku")
+    win.fill(background_color)
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    
+    for i in range(0,10):
+        if(i%3 == 0):
+            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i ,500 ), 4 )
+            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 4 )
+
+        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i ,500 ), 2 )
+        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2 )
+    pygame.display.update()
+    
+    for i in range(0, len(grid[0])):
+        for j in range(0, len(grid[0])):
+            if(0<grid[i][j]<10):
+                value = myfont.render(str(grid[i][j]), True, original_grid_element_color)
+                win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
+    pygame.display.update()
+
+
+def main():    
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption("Sudoku")
+    win.fill(background_color)
+    myfont = pygame.font.SysFont('Comic Sans MS', 35)
+    
+    for i in range(0,10):
+        if(i%3 == 0):
+            pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i ,500 ), 4 )
+            pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 4 )
+
+        pygame.draw.line(win, (0,0,0), (50 + 50*i, 50), (50 + 50*i ,500 ), 2 )
+        pygame.draw.line(win, (0,0,0), (50, 50 + 50*i), (500, 50 + 50*i), 2 )
+    pygame.display.update()
+    
+    for i in range(0, len(grid[0])):
+        for j in range(0, len(grid[0])):
+            if(0<grid[i][j]<10):
+                value = myfont.render(str(grid[i][j]), True, original_grid_element_color)
+                win.blit(value, ((j+1)*50 + 15, (i+1)*50 ))
+    pygame.display.update()
+            
+        
+    while True: 
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                insert(win, (pos[0]//50, pos[1]//50))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    diff='1'
+                    dif(win,diff)
+                if event.key == pygame.K_2:
+                    diff='2'
+                    dif(win,diff)
+                if event.key == pygame.K_3:
+                    diff='3'
+                    dif(win,diff)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+   
+main()
---
+++ pr.py
@@ -0,0 +1,79 @@
+import Sudoku as sd
+
+def list2dic(lista):
+    count=0
+    re=[]
+    res=[]
+    for i in lista:
+        re.append(i)
+        print(re,count)
+        if count != 8:
+            count+=1
+        else:
+            count=0
+            res.append(re)
+            re=[]
+            
+    return res
+
+numeros=[0,0,0,0,4,0,0,5,0,
+        5,0,4,6,0,3,8,0,0,
+        0,3,0,0,0,5,0,7,0,
+        0,6,2,0,0,0,0,3,0,
+        8,0,3,0,0,0,7,0,9,
+        0,7,0,0,0,0,6,2,0,
+        0,5,0,4,0,0,0,8,0,
+        0,0,6,2,0,7,5,0,3,
+        0,8,0,0,5,0,0,0,0]
+
+x=list2dic(numeros)
+
+#Ploting del grafo
+sudoku=sd.grafo_sudoku(numeros)
+sudoku.vs["label"] = ['Σ', 'Τ', 'Υ', 'Φ', 'Χ', 'Ψ', 'Ω', 'Ϊ', 'Ϋ',
+                    'ά', 'έ', 'ή', 'ί', 'ΰ', 'α', 'β', 'γ', 'δ',
+                    'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν',
+                    'ξ', 'ο', 'π', 'ρ', 'ς', 'σ', 'τ', 'υ', 'φ',
+                    'χ', 'ψ', 'ω', 'ϊ', 'ϋ', 'ό', 'ύ', 'ώ', 'Ϗ',
+                    'ϐ', 'ϑ', 'ϒ', 'ϓ', 'ϔ', 'ϕ', 'ϖ', 'ϗ', 'Ϙ',
+                    'ϙ', 'Ϛ', 'ϛ', 'Ϝ', 'ϝ', 'Ϟ', 'ϟ', 'Ϡ', 'ϡ',
+                    'Ϣ', 'ϣ', 'Ϥ', 'ϥ', 'Ϧ', 'ϧ', 'Ϩ', 'ϩ', 'Ϫ',
+                    'ϫ', 'Ϭ', 'Ϯ', 'ϯ', 'ϰ', 'ϱ', 'ϲ', 'ϳ', 'ϴ']
+
+layout = sudoku.layout_grid()
+global color_dict 
+color_dict= {0: "white", 1: "pink",2:"green",3:"yellow",4:"red",5:"purple",6:"orange",7:"light blue",8:"dark green",9:"Cadet Blue"}
+sudoku.vs["color"] = [color_dict[number] for number in sudoku.vs["numero"]]
+colores=["pink","green","yellow","red","purple","orange","light blue","dark green","Cadet Blue"]
+
+sudoku,listaSol,val= sd.solve_sudoku(sudoku)
+color_dict = {"white":0, "pink":1,"green": 2,"yellow":3,"red":4,"purple":5,"orange":6,"light blue":7,"dark green":8,"Cadet Blue":9}
+
+t_cl= []
+for i in listaSol:
+  t_cl.append(color_dict.get(i))
+
+count=0
+line=0
+for i in t_cl:
+  print(i, end=" ")
+  count+=1
+  if count % 3 == 0 and count != 9:
+    print('|',end=' ')
+  elif count % 9 == 0:
+    print()
+    line+=1
+    if line % 3 == 0 and line != 9:
+      print('------+-------+-------')
+    count=0
+  
+if val:
+    plot(sudoku, layout=layout)
+else:
+    y=input("p?")
+    if y=="y":
+        plot(sudoku, layout=layout)
+
+
+
+print('resultado:',x)
